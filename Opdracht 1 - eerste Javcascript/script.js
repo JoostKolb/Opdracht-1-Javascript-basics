4fb7fcd5ca35f21778476cd326355ac4f3d8c841
Joost Kolb <joostkolb@hotmail.com>
2021-04-02 17:42:10 +0200

    Add files via upload

---
+++ Opdracht 1 - eerste Javcascript/script.js
@@ -0,0 +1,41 @@
+// Opdracht 2:
+// Opdracht 3: Een tool waarmee je kunt debuggen
+// Opdracht 4: 
+console.log("Hallo winc Academy");
+
+// Opdracht 5:
+let name = "Joost";
+console.log(name);
+
+// Opdracht 6:
+//let if = "";
+//Dit werkt niet omdat if een functie is
+
+// Opdracht 7:
+let numbers = 4 + 4;
+let strings = "4" + "4";
+console.log(numbers);
+console.log(strings);
+
+// Opdracht 8:
+let numbersAddition = 4 + 4;
+let numbersSubtraction = 4 - 4;
+let numbersMultiplication = 4 * 4;
+let numbersDivision = 4 / 4;
+let numbersModulus = 4 % 4;
+
+console.log(numbersAddition);
+console.log(numbersSubtraction);
+console.log(numbersMultiplication);
+console.log(numbersDivision);
+console.log(numbersModulus);
+
+// Opdracht 9:
+let leeftijd = 26;
+console.log(typeof leeftijd);
+
+// Opdracht 10:
+let text = "26";
+console.log(typeof text);
+
+    // Opdracht 11:
